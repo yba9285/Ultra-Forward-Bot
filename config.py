435f67ddc8842418040d035450541d0e5cb3a13c
yba9285 <108516941+yba9285@users.noreply.github.com>
2024-09-10 13:00:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,8 +13,8 @@ class Config:
     API_HASH = os.environ.get("API_HASH", "2f10533d9068507d0c10bf1074527167")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
     BOT_SESSION = os.environ.get("BOT_SESSION", "forward-bot") 
-    DB_URL = os.environ.get("DB_URL", "")
-    DB_NAME = os.environ.get("DB_NAME", "madflixbotz")
+    DB_URL = os.environ.get("DB_URL", "mongodb+srv://metangevilas29:GyUAtsYGmMf4mGSH@cluster0.zq0ei.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    DB_NAME = os.environ.get("DB_NAME", "Mzbotz")
     OWNER_ID = [int(id) for id in os.environ.get("OWNER_ID", '5673859971').split()]
 
 
