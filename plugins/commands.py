64c6177c47ddb892bd66ff364ce16c4dbc78784d
yba9285 <108516941+yba9285@users.noreply.github.com>
2024-09-10 13:36:43 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -18,8 +18,8 @@ from pyrogram import Client, filters, enums, __version__ as pyrogram_version
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InputMediaDocument
 
 main_buttons = [[
-        InlineKeyboardButton('📢 Updates', url='https://t.me/Madflix_Bots'),
-        InlineKeyboardButton('💬 Support', url='https://t.me/MadflixBots_Support')
+        InlineKeyboardButton('📢 Updates', url='https://t.me/mzbotz'),
+        InlineKeyboardButton('💬 Support', url='https://t.me/mzbotz')
         ],[
         InlineKeyboardButton('🛠️ Help', callback_data='help'),
         InlineKeyboardButton('🩷 About', callback_data='about')
